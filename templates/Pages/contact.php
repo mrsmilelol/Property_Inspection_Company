8b581564fbb0f71e4f597bb31f3d33c84ab62807
tanjiawei@123 <jtan0179@student.monash.edu>
2022-08-13 22:17:16 +1000

    contact page  main contact area draft

--- templates/Pages/contact.php
+++ templates/Pages/contact.php
@@ -40,7 +40,63 @@ $this->layout = 'front';
         </div>
     </div>
 </div>
-<!-- Breadcrumbs Area End -->
+
+
+<!-- MAIN CONTENT AREA -->
+<div class="container">
+    <div class="row content-layout-wrapper align-items-start">
+        <div class="site-content col-lg-12 col-12 col-md-12" role="main">
+            <article id="post-5322" class="post-5322 page type-page status-publish hentry">
+                <div class="entry-content">
+                    <div class="vc_row wpb_row vc_row-fluid vc_custom_1648710086425 wd-rs-624551c31e088"><div class="wpb_column vc_column_container vc_col-sm-6 vc_col-lg-4 vc_col-md-4 wd-alignment-left wd-rs-61261191a5446"><div class="vc_column-inner vc_custom_1629884819677"><div class="wpb_wrapper">
+                                    <div id="wd-62c81a12e4417" class="title-wrapper wd-wpb set-mb-s reset-last-child  wd-rs-62c81a12e4417 wd-title-color-primary wd-title-style-default wd-width-100 text-left wd-off-anim wpb_animate_when_almost_visible wpb_flipInX flipInX wd-anim-name_flipInX vc_custom_1657281050020 wd-underline-colored">
+                                        <div class="liner-continer">
+                                            <h1 class="woodmart-title-container title  wd-font-weight- wd-fontsize-l" ><u>OUR</u> ADDRESS</h1>
+                                        </div>
+                                    </div>
+                                    <div class="vc_row wpb_row vc_inner vc_row-fluid"><div class="wpb_column vc_column_container vc_col-sm-12"><div class="vc_column-inner"><div class="wpb_wrapper">			<div class="info-box-wrapper">
+                                                        <div id="wd-6245d6486864d" class=" wd-rs-6245d6486864d wd-info-box woodmart-info-box wd-wpb text-left box-icon-align-top box-style- color-scheme- wd-bg-none box-title-large vc_custom_1648744033143"  >
+                                                            <div class="info-box-content">
+                                                                <h4 class="info-box-title title wd-font-weight- box-title-style-default font-primary wd-fontsize-xl">Melbourne</h4>						<div class="info-box-inner set-cont-mb-s reset-last-child">
+                                                                    <p>1680 Dandenong Road .Oakligh East.VIC</p>
+                                                                </div>
+                                                            </div>
+                                                            <style>#wd-6245d6486864d .info-box-icon {background-color:  !important;}#wd-6245d6486864d:hover .info-box-icon{background-color:  !important;}</style>				</div>
+                                                    </div>
+                                                    <div id="wd-624fd1926fdbd" class="wd-text-block wd-wpb reset-last-child wd-rs-624fd1926fdbd wd-width-100 text-left ">
+                                                        <p><span style="color: #333333;"><strong>Email: </strong></span>info@chelseafurniture.com.au</p>
+                                                        <p><span style="color: #333333;"><strong>Phone: </strong>037068 0002</span></p>
+                                                    </div>
+                                                </div></div></div></div>
+                                    <div class="wd-social-icons  woodmart-social-icons icons-design-bordered icons-size-default color-scheme-dark social-follow social-form-circle wd-off-anim wpb_animate_when_almost_visible wpb_flipInX flipInX wd-anim-name_flipInX wd-rs-62455e17b86ae text-left">
+                                        <a rel="noopener noreferrer nofollow" href="https://www.facebook.com/chelsea.furnitures/" target="_blank" class=" wd-social-icon social-facebook" aria-label="Facebook social link">
+                                            <span class="wd-icon"></span>
+                                        </a>
+                                        <a rel="noopener noreferrer nofollow" href="https://www.instagram.com/chelseafurniture_australia" target="_blank" class=" wd-social-icon social-instagram" aria-label="Instagram social link">
+                                            <span class="wd-icon"></span>
+                                        </a>
+                                        <a rel="noopener noreferrer nofollow" href="https://wa.me/+61404737301" target="_blank" class="whatsapp-desktop  wd-social-icon social-whatsapp" aria-label="WhatsApp social link">
+                                            <span class="wd-icon"></span>
+                                        </a>
+                                        <a rel="noopener noreferrer nofollow" href="https://wa.me/+61404737301" target="_blank" class="whatsapp-mobile  wd-social-icon social-whatsapp" aria-label="WhatsApp social link">
+                                            <span class="wd-icon"></span>
+                                        </a>
+                                    </div>
+                                    <div id="wd-62455e09de21f" class="title-wrapper wd-wpb set-mb-s reset-last-child  wd-rs-62455e09de21f wd-title-color-primary wd-title-style-default wd-width-100 text-left vc_custom_1648713232008 wd-underline-colored">
+                                        <div class="liner-continer">
+                                            <h4 class="woodmart-title-container title  wd-font-weight- wd-fontsize-l" ><u>CONTACT</u> US</h4>
+                                        </div>
+                                    </div>
+                                    <div id="wd-624559407424c" class="wd-text-block wd-wpb reset-last-child wd-rs-624559407424c wd-width-100 text-center vc_custom_1648712028623">
+                                        <p>Do you have any questions about our services and products? We&#8217;ll be happy to help you.</p>
+                                    </div>
+                                    <div id="wd-624fd1c8a47e4" class=" wd-rs-624fd1c8a47e4  wd-button-wrapper woodmart-button-wrapper wd-off-anim wpb_animate_when_almost_visible wpb_flipInY flipInY wd-anim-name_flipInY text-center"><a href="tel:0370680002" title="Call Us Now !" class="btn btn-scheme-light btn-scheme-hover-dark btn-style-3d btn-style-semi-round btn-size-extra-large">Call Us Now !</a></div></div></div></div><div class="wpb_column vc_column_container vc_col-sm-6 vc_col-lg-8 vc_col-md-8 wd-alignment-left wd-rs-6126118e36399"><div class="vc_column-inner"><div class="wpb_wrapper"><div class="wpb_gmaps_widget wpb_content_element vc_custom_1648712929115"><div class="wpb_wrapper"><div class="wpb_map_wraper"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3147.8605334907706!2d145.11741161584735!3d-37.91032244734883!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad66ac30cb98f31%3A0x9d94eccb268997bc!2s1680%20Dandenong%20Rd%2C%20Oakleigh%20East%20VIC%203166!5e0!3m2!1sen!2sau!4v1648712699658!5m2!1sen!2sau" width="800" height="600" style="border:0;" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div></div></div></div></div></div></div>
+                </div>
+            </article><!-- #post -->
+        </div><!-- .site-content -->
+    </div><!-- .main-page-wrapper -->
+</div> <!-- end row -->
+</div> <!-- end container --><!-- Breadcrumbs Area End -->
 <!-- Contact Area Area Start -->
 <!--<div class="contact-us-area section-padding2">-->
 <!--    <div class="container">-->
