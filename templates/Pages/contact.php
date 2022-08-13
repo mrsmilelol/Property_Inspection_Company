9954be361e4646ae97265c286d62ddf80bfb98b5
tanjiawei@123 <jtan0179@student.monash.edu>
2022-08-13 22:08:06 +1000

    some changes in the contact page

--- templates/Pages/contact.php
+++ templates/Pages/contact.php
@@ -42,68 +42,68 @@ $this->layout = 'front';
 </div>
 <!-- Breadcrumbs Area End -->
 <!-- Contact Area Area Start -->
-<div class="contact-us-area section-padding2">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h1 class="heading-title">Customer service - Contact us</h1>
-                <form action="mail.php" method="POST" class="contact-form-box">
-                    <fieldset>
-                        <h3 class="page-subheading">send a message</h3>
-                        <div class="row">
-                            <div class="col-xs-12 col-md-3">
-                                <div class="form-group">
-                                    <label>Subject Heading</label>
-                                    <div class="shop-select" id="uniform-id_contact">
-                                        <select name="id_contact" id="id_contact">
-                                            <option selected="selected">-- Choose --</option>
-                                            <option>Customer service</option>
-                                            <option>Webmaster</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label>Email address</label>
-                                    <div class="shop-select">
-                                        <input type="email" value="" name="email" id="email">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label>Order reference</label>
-                                    <div class="shop-select">
-                                        <input type="text" value="" id="id_order" name="id_order">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label>Attach File</label>
-                                    <input type="hidden" name="MAX_FILE_SIZE" value="8388608">
-                                    <div id="uniform-fileUpload" class="uploader">
-                                        <input type="file" class="form-control" id="fileUpload" name="fileUpload">
-                                        <span class="filename">No file selected</span>
-                                        <span class="action">Choose File</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xs-12 col-md-9">
-                                <div class="form-group">
-                                    <label for="message">Message</label>
-                                    <div class="shop-select">
-                                        <textarea name="message" id="message"></textarea>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="submit">
-                            <button class="show-pagi" name="submitMessage" id="submitMessage" type="submit">
-                                <span>Send <i class="fa fa-chevron-right right"></i> </span>
-                            </button>
-                        </div>
-                    </fieldset>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
+<!--<div class="contact-us-area section-padding2">-->
+<!--    <div class="container">-->
+<!--        <div class="row">-->
+<!--            <div class="col-md-12">-->
+<!--                <h1 class="heading-title">Customer service - Contact us</h1>-->
+<!--                <form action="mail.php" method="POST" class="contact-form-box">-->
+<!--                    <fieldset>-->
+<!--                        <h3 class="page-subheading">send a message</h3>-->
+<!--                        <div class="row">-->
+<!--                            <div class="col-xs-12 col-md-3">-->
+<!--                                <div class="form-group">-->
+<!--                                    <label>Subject Heading</label>-->
+<!--                                    <div class="shop-select" id="uniform-id_contact">-->
+<!--                                        <select name="id_contact" id="id_contact">-->
+<!--                                            <option selected="selected">-- Choose --</option>-->
+<!--                                            <option>Customer service</option>-->
+<!--                                            <option>Webmaster</option>-->
+<!--                                        </select>-->
+<!--                                    </div>-->
+<!--                                </div>-->
+<!--                                <div class="form-group">-->
+<!--                                    <label>Email address</label>-->
+<!--                                    <div class="shop-select">-->
+<!--                                        <input type="email" value="" name="email" id="email">-->
+<!--                                    </div>-->
+<!--                                </div>-->
+<!--                                <div class="form-group">-->
+<!--                                    <label>Order reference</label>-->
+<!--                                    <div class="shop-select">-->
+<!--                                        <input type="text" value="" id="id_order" name="id_order">-->
+<!--                                    </div>-->
+<!--                                </div>-->
+<!--                                <div class="form-group">-->
+<!--                                    <label>Attach File</label>-->
+<!--                                    <input type="hidden" name="MAX_FILE_SIZE" value="8388608">-->
+<!--                                    <div id="uniform-fileUpload" class="uploader">-->
+<!--                                        <input type="file" class="form-control" id="fileUpload" name="fileUpload">-->
+<!--                                        <span class="filename">No file selected</span>-->
+<!--                                        <span class="action">Choose File</span>-->
+<!--                                    </div>-->
+<!--                                </div>-->
+<!--                            </div>-->
+<!--                            <div class="col-xs-12 col-md-9">-->
+<!--                                <div class="form-group">-->
+<!--                                    <label for="message">Message</label>-->
+<!--                                    <div class="shop-select">-->
+<!--                                        <textarea name="message" id="message"></textarea>-->
+<!--                                    </div>-->
+<!--                                </div>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                        <div class="submit">-->
+<!--                            <button class="show-pagi" name="submitMessage" id="submitMessage" type="submit">-->
+<!--                                <span>Send <i class="fa fa-chevron-right right"></i> </span>-->
+<!--                            </button>-->
+<!--                        </div>-->
+<!--                    </fieldset>-->
+<!--                </form>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
 <!-- Contact Area Area End -->
 <!--Brand Area Start-->
 <div class="brand-area section-padding2">
