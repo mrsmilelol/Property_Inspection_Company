6bca9382ed7f9ee18cddfca08d0517fc38cc0322
Shaunna Wee <swee0009@student.monash.edu>
2022-08-16 17:08:39 +1000

    fixed table appearance for add new products button

--- templates/Products/index.php
+++ templates/Products/index.php
@@ -13,12 +13,10 @@ echo $this->Html->script('//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.j
 <div class="container-fluid">
         <!-- Page Heading -->
                 <div class="card shadow mb-4">
-                    <div class="card-header py-3">
-                        <h6 class="m-0 font-weight-bold text-primary"><?= __('Products') ?></h6>
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4 card-header">
+                        <h1 class="h3 mb-0 text-gray-800"><?= __('Products') ?></h1>
                         <a href="<?= $this->Url->build(['action' => 'add']) ?>" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                                 class="fas fa-plus fa-sm text-white-50"></i> Add new product</a>
-
-
                     </div>
                     <div class="card-body">
                         <div class="table-responsive">
